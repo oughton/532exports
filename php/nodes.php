2db143fe06d5ad9897dac3a8b6b9643f83fe4869
Stephen <stephennancekivell@gmail.com>
2011-06-09 14:58:05 +1200

    with static node parents

--- php/nodes.php
+++ php/nodes.php
@@ -1,14 +1,21 @@
 <?php
+
+class node {
+}
+
 if (($handle = fopen("../datasets/nodes.csv", "r"))!== FALSE){
     $count=0;
     while(($data = fgetcsv($handle,0,",")) !== FALSE){
         $count+=1;
         $dst = array();
-        for ($i=1; $i < count($data); $i++){
+        $n = new node();
+        $n->parent = $data[1];
+        for ($i=2; $i < count($data); $i++){
             $dst[] = $data[$i];
-        }   
+        }
+        $n->children = $dst;
 
-        if (substr($data[0],0,2) != "\\\\") $output[$data[0]] = $dst;
+        if (substr($data[0],0,2) != "\\\\") $output[$data[0]] = $n;
     }
     fclose($handle);
     echo json_encode($output);
